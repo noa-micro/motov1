b4f60a2ef44d0041deb527de18c9baa656a4c58b
noa-micro <121046810+noa-micro@users.noreply.github.com>
2022-12-31 16:59:42 +0100

    añadido cambio de sentido si choca

--- main.ts
+++ main.ts
@@ -1,12 +1,50 @@
 input.onButtonPressed(Button.A, function () {
+    direccion = 0
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
     wuKong.setAllMotor(-100, -100)
 })
 input.onButtonPressed(Button.AB, function () {
     wuKong.stopAllMotor()
 })
 input.onButtonPressed(Button.B, function () {
+    direccion = 1
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
     wuKong.setAllMotor(100, 100)
 })
+input.onGesture(Gesture.ThreeG, function () {
+    if (direccion == 0) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+        wuKong.setAllMotor(100, 100)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        wuKong.setAllMotor(-100, -100)
+    }
+})
+let direccion = 0
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
 	
